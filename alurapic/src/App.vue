8249ee9ec468f89ba513377ece858e3059db5242
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-10 14:43:10 -0300

    Aula 4: Finalizada

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,36 +1,63 @@
 <template>
-<div>
-  <h1> {{titulo}} </h1>
-  <ul> 
-    <li v-for="foto of fotos" v-bind:key="foto">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="titulo">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos" v-bind:key="foto">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
-  </ul>
-</div>
-
+    </ul>
+  </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
- 
-  data() {
+
+  components: {
+
+    'meu-painel': Painel
+  },
+
+  data () {
     return {
-      titulo: "Alurapic",
+      titulo: 'Alurapic', 
+
       fotos: []
     }
   },
-
-
   created() {
 
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http
+      .get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
   }
- 
 }
 </script>
 
 <style>
+.titulo {
+  text-align: center;
+}
 
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
 </style>
