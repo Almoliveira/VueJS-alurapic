1c60f4ab0b02bfdb7e1561bf12003dc687494dbc
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-14 03:58:58 -0300

    Aula 1: Atividade 5 Finalizada

--- alurapic/src/directives/Transform.js
+++ alurapic/src/directives/Transform.js
@@ -1,14 +1,26 @@
-// alurapic/src/directives/Transform.js
-
 import Vue from 'vue';
 
 Vue.directive('meu-transform', {
 
     bind(el, binding, vnode) {
+
       let current = 0;
+
       el.addEventListener('dblclick', function() {
-        current+=90;
+
+        let incremento = 90;
+        let animate = false;
+
+        if(binding.value) {
+            incremento = binding.value.incremento
+            animate = binding.value.animate 
+
+        }
+
+        current+=incremento;
         this.style.transform = `rotate(${current}deg)`;
+        if (animate) this.style.transition = "transform 0.5s";
+
       });
     }
 
