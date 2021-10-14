e165b5b68b6389ca33db2f4cc42991c56a970342
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-14 04:15:31 -0300

    Aula 1: Finalizada

--- alurapic/src/components/home/home.vue
+++ alurapic/src/components/home/home.vue
@@ -13,7 +13,9 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-         <imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform.animate.reverse="15"/>
+         <!-- alurapic/src/directives/Transform.js -->
+
+<imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform:scale="1.2"/>
 
           <meu-botao
             tipo="button"
